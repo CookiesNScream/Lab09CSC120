889e8a2dfa887ab82965a947e19f1bbcb873ff83
Arya Venkatesan <ar6165@gmail.com>
2022-07-18 20:29:49 -0400

    Added Board Display

---
+++ tictactoe.py
@@ -0,0 +1,7 @@
+#stage 1
+line0 = ["-", "-", "-"]
+line1 = ["-", "-", "-"]
+line2 = ["-", "-", "-"]
+def display():
+  print("\n Printing board...", "\n", line0, "\n", line1, "\n", line2, "\n")
+display()
